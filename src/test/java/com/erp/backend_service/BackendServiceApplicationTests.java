de3d9605263f8038a2e99a85c982725b565bb807
dunghd <hoangdinhdung0205@gmail.com>
2026-08-17 14:21:52 +0700

    feat: initial backend source

---
+++ src/test/java/com/erp/backend_service/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.backend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
